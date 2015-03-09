9d50faa7b4e025ff656d83c8def628d90d146ed2
Gabor Molnar <gabormolnar92@gmail.com>
2015-03-09 21:11:14 +0100

    Design #7

--- app/views/_frontend/footer.blade.php
+++ app/views/_frontend/footer.blade.php
@@ -1,9 +1,11 @@
-
 <footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <p>{{date('Y')}} © {{Setting::get('site-title')}}</p>
+
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>{{date('Y')}} © {{Setting::get('site-title')}}</p>
+                </div>
             </div>
         </div>
     </div>
--- app/views/_frontend/menu.blade.php
+++ app/views/_frontend/menu.blade.php
@@ -1,14 +1,16 @@
 @foreach ($items as $item)
-<li class=" @if($item->hasChildren())dropdown @endif @if($item->link->path['url'] == Request::url()) active @endif">
-@if(!$item->hasChildren())
-<a href="{{ $item->link->path['url'] }}">{{$item->title }} </a>
-@else
-<a href="{{ $item->link->path['url'] }}">{{$item->title }} <span class="caret"></span></a>
-@endif
-@if ($item->hasChildren())
-<ul>
-    @include('_frontend.menu', array('items' => $item->children()))
-</ul>
-@endif
-</li >
+    <li class=" @if($item->hasChildren())dropdown @endif @if($item->link->path['url'] == Request::url()) active @endif">
+        @if(!$item->hasChildren())
+            <a href="{{ $item->link->path['url'] }}">{{$item->title }} </a>
+        @else
+            <a href="{{ $item->link->path['url'] }}">{{$item->title }} <span class="caret"></span></a>
+        @endif
+        @if ($item->hasChildren())
+
+            <ul class="container">
+                @include('_frontend.menu', array('items' => $item->children()))
+            </ul>
+
+        @endif
+    </li>
 @endforeach
